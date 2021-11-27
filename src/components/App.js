484e8817ac9115d0c511f4674044f8faad2fd704
andrey-71 <and_go1991@mail.ru>
2021-11-27 15:33:53 +0300

    feat: создать контекст пользователя, импортировать и подключить в App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import {CurrentUserContext} from "../contexts/CurrentUserContext";
 import Header from './Header';
 import Main from './Main';
 import Footer from './Footer';
@@ -18,7 +19,6 @@ function App() {
       })
       .catch(err => console.log(`При загрузке данных пользователя произошла ошибка: ${err}`));
   }, []);
-  console.log(currentUser);
   // Начальные состояния для закрытых попапов
   const [isEditAvatarPopupOpen, setIsEditAvatarPopupOpen] = React.useState(false);
   const [isEditProfilePopupOpen, setIsEditProfilePopupOpen] = React.useState(false);
@@ -71,137 +71,139 @@ function App() {
   }
 
   return (
-    <div className="container">
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="container">
 
-      <Header />
-      <Main
-        onEditAvatar = {handleEditAvatarClick}
-        onEditProfile = {handleEditProfileClick}
-        onAddPlace = {handleAddPlaceClick}
-        onCardClick = {handleCardClick}
-      />
-      <Footer />
+        <Header />
+        <Main
+          onEditAvatar = {handleEditAvatarClick}
+          onEditProfile = {handleEditProfileClick}
+          onAddPlace = {handleAddPlaceClick}
+          onCardClick = {handleCardClick}
+        />
+        <Footer />
 
-      {/* Popup edit user info */}
-      <PopupWithForm
-        name = "edit-profile"
-        title = "Редактировать профиль"
-        textButton = "Сохранить"
-        isOpen = {isEditProfilePopupOpen}
-        onPopupClick = {handleOverlayClick}
-        onClose = {closeAllPopups}
-      >
-        <>
-          <label className="popup__input-element">
-            <input
-              type="text"
-              placeholder="Ваше имя"
-              className="popup__input popup__input_type_profile-name"
-              id="input-profile-name"
-              name="nameUser"
-              required
-              min={2}
-              max={40}
-            />
-            <span
-              className="popup__input-error"
-              id="input-profile-name-error"
-            ></span>
-          </label>
-          <label className="popup__input-element">
-            <input
-              type="text"
-              placeholder="Чем занимаетесь"
-              className="popup__input popup__input_type_profile-job"
-              id="input-profile-job"
-              name="infoUser"
-              required
-              min={2}
-              max={200}
-            />
-            <span
-              className="popup__input-error"
-              id="input-profile-job-error"
-            ></span>
-          </label>
-        </>
+        {/* Popup edit user info */}
+        <PopupWithForm
+          name = "edit-profile"
+          title = "Редактировать профиль"
+          textButton = "Сохранить"
+          isOpen = {isEditProfilePopupOpen}
+          onPopupClick = {handleOverlayClick}
+          onClose = {closeAllPopups}
+        >
+          <>
+            <label className="popup__input-element">
+              <input
+                type="text"
+                placeholder="Ваше имя"
+                className="popup__input popup__input_type_profile-name"
+                id="input-profile-name"
+                name="nameUser"
+                required
+                min={2}
+                max={40}
+              />
+              <span
+                className="popup__input-error"
+                id="input-profile-name-error"
+              ></span>
+            </label>
+            <label className="popup__input-element">
+              <input
+                type="text"
+                placeholder="Чем занимаетесь"
+                className="popup__input popup__input_type_profile-job"
+                id="input-profile-job"
+                name="infoUser"
+                required
+                min={2}
+                max={200}
+              />
+              <span
+                className="popup__input-error"
+                id="input-profile-job-error"
+              ></span>
+            </label>
+          </>
         </PopupWithForm>
 
-      {/* Popup edit user avatar */}
-      <PopupWithForm
-        name = "edit-avatar"
-        title = "Обновить аватар"
-        textButton = "Сохранить"
-        isOpen = {isEditAvatarPopupOpen}
-        onPopupClick = {handleOverlayClick}
-        onClose = {closeAllPopups}
-      >
-        <>
+        {/* Popup edit user avatar */}
+        <PopupWithForm
+          name = "edit-avatar"
+          title = "Обновить аватар"
+          textButton = "Сохранить"
+          isOpen = {isEditAvatarPopupOpen}
+          onPopupClick = {handleOverlayClick}
+          onClose = {closeAllPopups}
+        >
+          <>
+            <label className="popup__input-element">
+              <input
+                type="url"
+                placeholder="Ссылка на картинку"
+                className="popup__input popup__input_type_avatar-link"
+                id="input-avatar-link"
+                name="avatarUser"
+                required
+              />
+              <span className="popup__input-error" id="input-avatar-link-error"></span>
+            </label>
+          </>
+        </PopupWithForm>
+
+        {/* Popup add cards */}
+        <PopupWithForm
+          name = "add-card"
+          title = "Новое место"
+          textButton = "Создать"
+          isOpen = {isAddPlacePopupOpen}
+          onPopupClick = {handleOverlayClick}
+          onClose = {closeAllPopups}
+        >
+          <label className="popup__input-element">
+            <input
+              type="text"
+              placeholder="Название"
+              className="popup__input popup__input_type_card-name"
+              id="input-card-name"
+              name="name"
+              required
+              min={2}
+              max={30}
+            />
+            <span className="popup__input-error" id="input-card-name-error"></span>
+          </label>
           <label className="popup__input-element">
             <input
               type="url"
               placeholder="Ссылка на картинку"
-              className="popup__input popup__input_type_avatar-link"
-              id="input-avatar-link"
-              name="avatarUser"
+              className="popup__input popup__input_type_card-link"
+              id="input-card-link"
+              name="link"
               required
             />
-            <span className="popup__input-error" id="input-avatar-link-error"></span>
+            <span className="popup__input-error" id="input-card-link-error"></span>
           </label>
-        </>
-      </PopupWithForm>
+        </PopupWithForm>
 
-      {/* Popup add cards */}
-      <PopupWithForm
-        name = "add-card"
-        title = "Новое место"
-        textButton = "Создать"
-        isOpen = {isAddPlacePopupOpen}
-        onPopupClick = {handleOverlayClick}
-        onClose = {closeAllPopups}
-      >
-        <label className="popup__input-element">
-          <input
-            type="text"
-            placeholder="Название"
-            className="popup__input popup__input_type_card-name"
-            id="input-card-name"
-            name="name"
-            required
-            min={2}
-            max={30}
-          />
-          <span className="popup__input-error" id="input-card-name-error"></span>
-        </label>
-        <label className="popup__input-element">
-          <input
-            type="url"
-            placeholder="Ссылка на картинку"
-            className="popup__input popup__input_type_card-link"
-            id="input-card-link"
-            name="link"
-            required
-          />
-          <span className="popup__input-error" id="input-card-link-error"></span>
-        </label>
-      </PopupWithForm>
+        {/* Popup delete card */}
+        <PopupWithForm
+          name = "delete-card"
+          title = "Вы уверены?"
+          textButton = "Да"
+          onPopupClick = {handleOverlayClick}
+        ></PopupWithForm>
 
-      {/* Popup delete card */}
-      <PopupWithForm
-        name = "delete-card"
-        title = "Вы уверены?"
-        textButton = "Да"
-        onPopupClick = {handleOverlayClick}
-      ></PopupWithForm>
+        {/* Popup view card */}
+        <ImagePopup
+          card = {selectedCard}
+          onPopupClick = {handleOverlayClick}
+          onClose = {closeAllPopups}
+        />
 
-      {/* Popup view card */}
-      <ImagePopup
-        card = {selectedCard}
-        onPopupClick = {handleOverlayClick}
-        onClose = {closeAllPopups}
-      />
-
-    </div>
+      </div>
+    </CurrentUserContext.Provider>
   );
 }
 
