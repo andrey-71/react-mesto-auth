3a8dd01747d17ca33638ec03afe59441a3cba294
andrey-71 <and_go1991@mail.ru>
2021-12-02 10:41:27 +0300

    fix: присвоить форме каждого попапа уникальное значение атрибута name

--- src/components/PopupWithForm.js
+++ src/components/PopupWithForm.js
@@ -11,7 +11,7 @@ function PopupWithForm(props) {
         <h2 className="popup__title">{props.title}</h2>
         <form
           className={`popup__form popup__form_type_${props.name}`}
-          name="form"
+          name={`form-${props.name}`}
           onSubmit={props.onSubmit}
         >
           {props.children}
