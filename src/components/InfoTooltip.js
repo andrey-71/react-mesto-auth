1110fc96aac5e403f464ff98b1dd9916b88df112
andrey-71 <and_go1991@mail.ru>
2021-12-11 20:31:44 +0300

    feat: создать компонент InfoTooltip

---
+++ src/components/InfoTooltip.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+function InfoTooltip(props) {
+
+  return (
+    <div className={`popup popup_active popup_type_info-tooltip`}
+         onMouseDown={props.onPopupClick}
+    >
+      <div className='popup__container'>
+        <div
+          className={`popup__image popup__image_type_${props.status ? 'success' : 'error'}`}
+        ></div>
+        <p className='popup__status-text'>
+          {props.status
+            ?
+            'Вы успешно зарегистрировались!'
+            :
+            'Что-то пошло не так! Попробуйте ещё раз.'
+          }
+        </p>
+        <button
+          className='popup__close'
+          type='button'
+          onClick={props.onClose}
+        />
+      </div>
+
+
+    </div>
+  )
+
+}
+
+export default InfoTooltip;
--- src/components/Register.js
+++ src/components/Register.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import auth from '../utils/auth';
 import {Link, useNavigate} from 'react-router-dom';
+import InfoTooltip from './InfoTooltip';
 
 
 function Register() {
   const navigate = useNavigate();
 
-  // Стейт-переменные инпутов
+  // Стейт-переменные
+  // - инпутов
   const [email, setEmail] = React.useState('');
   const [password, setPassword] = React.useState('');
+  // - регистрации
+  const [isRegistered, setIsRegistered] = React.useState(false);
 
   // Обработчики изменения сотсояния инпутов
   function handleChangeEmail(evt) {
@@ -33,36 +37,38 @@ function Register() {
 
 
   return (
-    <div className='auth auth_type_registr container__auth'>
-      <h2 className='auth__title'>Регистрация</h2>
-      <form
-        className='auth__form auth__form_type_registr'
-        onSubmit={handleSubmit}
-      >
-        <input
-          className='auth__input'
-          name='emailUser'
-          type='email'
-          placeholder='Email'
-          required
-          onChange={handleChangeEmail}
-        />
-        <input
-          className='auth__input'
-          name='passwordUser'
-          type='password'
-          placeholder='Пароль'
-          required
-          onChange={handleChangePassword}
-        />
-        <button className='auth__submit-button' type='submit'>Зарегистрироваться</button>
-      </form>
-      <div className='auth__sign-in'>
-        <p className='auth__registration-request-text'>Уже зарегистрированы?</p>
-        <Link to='/sign-in' className='auth__login-link'>Войти</Link>
+    <>
+      <div className='auth auth_type_registr container__auth'>
+        <h2 className='auth__title'>Регистрация</h2>
+        <form
+          className='auth__form auth__form_type_registr'
+          onSubmit={handleSubmit}
+        >
+          <input
+            className='auth__input'
+            name='emailUser'
+            type='email'
+            placeholder='Email'
+            required
+            onChange={handleChangeEmail}
+          />
+          <input
+            className='auth__input'
+            name='passwordUser'
+            type='password'
+            placeholder='Пароль'
+            required
+            onChange={handleChangePassword}
+          />
+          <button className='auth__submit-button' type='submit'>Зарегистрироваться</button>
+        </form>
+        <div className='auth__sign-in'>
+          <p className='auth__registration-request-text'>Уже зарегистрированы?</p>
+          <Link to='/sign-in' className='auth__login-link'>Войти</Link>
+        </div>
       </div>
-
-    </div>
+      <InfoTooltip status={isRegistered}/>
+    </>
   )
 }
 
